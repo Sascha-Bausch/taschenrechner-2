39fcc54ad77bfe31b423b747649245a2f56b0e77
Sascha-Bausch <85745172+Sascha-Bausch@users.noreply.github.com>
2022-01-08 14:30:30 +0100

    Add files via upload

---
+++ src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>Willkommen in der wunderbaren Welt der Mathematik</h1>
+    <h2>Meine erste kleine Taschenrechner-App</h2>
+    <h3>&copy; 2022<br />by Sascha Bausch</h3>
+  </div>
+</template>
+
+<style scoped>
+h2 {
+  font-family: cursive;
+}
+h3 {
+  margin-top: 150px;
+  font-family: cursive;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <img alt="SB logo" src="../assets/logo_sb.jpg" />
+    <Taschenrechner />
+    <!-- msg="Willkommen in der wunderbaren Welt der Mathematik" -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Taschenrechner from "@/components/Taschenrechner.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Taschenrechner,
+  },
+};
+</script>
+
+<style scoped>
+div {
+  font-family: monospace;
+}
+</style>
